1e318ce602abe49b8e6630f25657fbbb4debdd74
Chris Newbold <chris@newboldcreative.com>
2022-10-17 16:34:20 +1100

    update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://cluster0.hbpincv.mongodb.net/mern-shopping", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern-shopping", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
