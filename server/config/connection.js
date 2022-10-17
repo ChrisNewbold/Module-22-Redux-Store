baef63ab835aca0b4fba8a9719ba42a5662e2432
Chris Newbold <chris@newboldcreative.com>
2022-10-17 17:22:11 +1100

    update resolvers

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cluster0.hbpincv.mongodb.net/myFirstDatabase', {
-  // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern-shopping", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern-shopping", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
